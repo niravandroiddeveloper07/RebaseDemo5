abf7a0718606f5fe730dac2b90c4e5eeb1ffe40f
Nirav <nirav.sculptsoft@gmail.com>
2022-05-22 20:19:37 +0530

    > second commit

--- app/src/main/java/com/example/rebasedemo5/MainActivity.kt
+++ app/src/main/java/com/example/rebasedemo5/MainActivity.kt
@@ -2,7 +2,7 @@ package com.example.rebasedemo5
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+// second commit
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
